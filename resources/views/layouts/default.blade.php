498e2857b021a96c0016e08ed142545bcf932f20
zfs5220 <zfs5220@qq.com>
2020-10-24 15:47:49 +0800

    基础页面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>@yield('title','weibo app')</title>
+        <title>@yield('title','weibo app') - laravel 学习</title>
     </head>
     <body>
         @yield('content')
