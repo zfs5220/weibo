e0e3f090c3d099f2ae045c0002b5843b9a632efd
zfs5220 <zfs5220@qq.com>
2020-10-25 10:49:17 +0800

    用户显示页面

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,4 +36,13 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /*
+    Gravatar 为 “全球通用头像”，当你在 Gravatar 的服务器上放置了自己的头像后，可通过将自己的 Gravatar 登录邮箱进行 MD5 转码，并与 Gravatar 的 URL 进行拼接来获取到自己的 Gravatar 头像。
+    */
+    public function gravatar($size = '100')
+    {
+        $hash = md5(strtolower(trim($this->attributes['email'])));
+        return "http://www.gravatar.com/avatar/$hash?s=$size";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,16 @@ Route::get('/help', 'StaticPagesController@help') -> name("help");
 Route::get('/about', 'StaticPagesController@about') -> name("about");
 
 Route::get('signup', 'UsersController@create')->name('signup');
+
+/*
+Route::resource方法将遵从 RESTful 架构为用户资源生成路由，等同于以下写法
+
+Route::get('/users', 'UsersController@index')->name('users.index');
+Route::get('/users/create', 'UsersController@create')->name('users.create');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+Route::post('/users', 'UsersController@store')->name('users.store');
+Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+*/
+Route::resource('users', 'UsersController');
