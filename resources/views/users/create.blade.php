99d44723ee0ba9400c2c014bdd7a11e4c0d20830
zfs5220 <zfs5220@qq.com>
2020-10-25 11:28:37 +0800

    中文的错误提示信息

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,31 +8,34 @@
             <h5>注册</h5>
             </div>
             <div class="card-body">
-            <form method="POST" action="{{ route('users.store') }}">
-                <!--Laravel 为了安全考虑，会让我们提供一个 token（令牌）来防止我们的应用受到 CSRF（跨站请求伪造）的攻击-->
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="name">名称：</label>
-                    <input type="text" name="name" class="form-control" value="{{ old('name') }}">
-                </div>
 
-                <div class="form-group">
-                    <label for="email">邮箱：</label>
-                    <input type="text" name="email" class="form-control" value="{{ old('email') }}">
-                </div>
+                @include('shared._errors')
 
-                <div class="form-group">
-                    <label for="password">密码：</label>
-                    <input type="password" name="password" class="form-control" value="{{ old('password') }}">
-                </div>
+                <form method="POST" action="{{ route('users.store') }}">
+                    <!--Laravel 为了安全考虑，会让我们提供一个 token（令牌）来防止我们的应用受到 CSRF（跨站请求伪造）的攻击-->
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <label for="name">名称：</label>
+                        <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+                    </div>
 
-                <div class="form-group">
-                    <label for="password_confirmation">确认密码：</label>
-                    <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
-                </div>
+                    <div class="form-group">
+                        <label for="email">邮箱：</label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+                    </div>
 
-                <button type="submit" class="btn btn-primary">注册</button>
-            </form>
+                    <div class="form-group">
+                        <label for="password">密码：</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">确认密码：</label>
+                        <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">注册</button>
+                </form>
             </div>
         </div>
     </div>
