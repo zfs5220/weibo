7fba6e1b6f0e599f58ace487d4f663a0c0d7dca0
zfs5220 <zfs5220@qq.com>
2020-10-25 13:38:43 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,7 @@ Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
 */
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
