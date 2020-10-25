18ea49a913c07978351090a9f78b48490b1eec2a
zfs5220 <zfs5220@qq.com>
2020-10-25 11:38:55 +0800

    完成用户注册功能

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,8 @@
     </head>
     <body>
         @include('layouts._header')
-        <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @include('shared._messages')
             @yield('content')
             @include('layouts._footer')
         </div>
